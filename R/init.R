3b100c25628e99ca1851510cc3876d662358758e
johndharrison <johndharrison0@gmail.com>
2016-10-20 16:57:55 -0700

    Fix print method for proxy. Add additonal fields to proxy object.

--- R/init.R
+++ R/init.R
@@ -3,31 +3,27 @@
 #' @param ipAddress ip address of BrowserMob Proxy standalone
 #' @param bmpPort port BrowserMob Proxy is listening on
 #' @param port port you wish to use for proxy traffic
+#' @param openPort call the openPort function to create a new proxy on the
+#'    BMP server.
 #'
 #' @return an object of type proxy see \code{\link{proxy}}
 #' @export
 #'
 #' @examples
 
-proxy <- function(ipAddress = "http://localhost", bmpPort, port){
+proxy <- function(ipAddress = "http://localhost", bmpPort, port,
+                  openPort = TRUE){
   proxy <- list(url = paste(ipAddress, bmpPort, sep = ":"),
-                port = port)
+                port = port,
+                baseip = ipAddress,
+                bmpPort = bmpPort)
   class(proxy) <- "proxy"
-  openPort(proxy)
+  if(openPort){openPort(proxy)}
   invisible(proxy)
 }
 
-#' Documentation of proxy object
-#'
-#' doc for proxy
-#' @name proxy
-#' @examples
-#' \dontrun{
-#' 
-#' }
-NULL
-
-proxy.print <- function(x, ...){
+#' @export
+print.proxy <- function(x, ...){
   cat("Proxy ip address:", x$url, "\n")
   cat("Proxy listening for traffic on port:", x$port, "\n")
 }
