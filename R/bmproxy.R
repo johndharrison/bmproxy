2811de15bd70d0762d7ca253040441622432dfef
johndharrison <johndharrison0@gmail.com>
2016-10-20 12:01:15 -0700

    Add package documentation

---
+++ R/bmproxy.R
@@ -0,0 +1,10 @@
+
+#' Wrapper around the 'BrowserMob' Proxy REST API for R
+#'
+#' Wrapper around the 'BrowserMob Proxy' REST API for R.
+#' 'BrowserMob Proxy' is a utility to help web developers watch and
+#' manipulate network traffic from their AJAX applications.
+#'
+#' @docType package
+#' @name bmproxy
+NULL
