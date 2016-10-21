3b800ce4d6bd27df600eb43e30e469d437ff2c99
johndharrison <johndharrison0@gmail.com>
2016-10-21 15:55:05 -0700

    Add HAR function documentation

--- R/harDoc.R
+++ R/harDoc.R
@@ -1,39 +1,61 @@
-#' createHAR
+#' Create HAR
 #'
-#'createHAR
+#' Creates a new HAR attached to the proxy and returns the HAR content if 
+#'    there was a previous HAR.
 #'
 #' @template proxy
+#' @param ref The string name of The first page ref that should be used in 
+#'    the HAR. Optional if NULL defaults to "Page 1".
+#' @param title The title of first HAR page. Optional, default to ref
+#' @paran captureHeaders Boolean, capture headers or not. Optional, 
+#'    default to FALSE.
+#' @param captureContent Boolean, capture content bodies or not. Optional,
+#'    default to FALSE.
+#' @param captureBinaryContent Boolean, capture binary content or not. 
+#'    Optional, default to FALSE.
 #' @template ellipsis
 #' @family har functions
 #' @export
+#' @return Returns a list with the response status code and har content.
+#'    Returns status 204 when creating a har with a particular name the 
+#'    first time and har = NULL. Returns 200 and the previous har when 
+#'    creating one with the same name 
 #'
 #' @example /inst/examples/docs/har.R
 #' @name createHAR
 NULL
 
 
-#' newPage
+#' Create new page
 #'
-#'newPage
+#' Starts a new page on the existing HAR.
 #'
 #' @template proxy
+#' @param ref The string name of the first page ref that should be used in 
+#'    the HAR. Optional, default to "Page N" where N is the next page 
+#'    number (zero indexed so 1st page is page 0).
+#' @param title The title of new HAR page. Optional, default to ref.
 #' @template ellipsis
 #' @family har functions
 #' @export
+#' @template retProxy
 #'
 #' @example /inst/examples/docs/har.R
 #' @name newPage
 NULL
 
 
-#' getHAR
+#' Get HAR
 #'
-#'getHAR
+#' Returns the JSON/HAR content representing all the HTTP traffic passed 
+#'    through the proxy (provided you have already created the HAR with 
+#'    \code{\link{createHAR}})
 #'
 #' @template proxy
 #' @template ellipsis
 #' @family har functions
 #' @export
+#' @return Returns the HAR as parsed JSON content
 #'
 #' @example /inst/examples/docs/har.R
 #' @name getHAR
