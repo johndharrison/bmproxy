0af4b446b77d18252af9e0f057de4c51299c7205
johndharrison <johndharrison0@gmail.com>
2016-10-21 09:42:42 -0700

    Add session function tests

---
+++ tests/testthat/test-session_functions.R
@@ -0,0 +1,48 @@
+context("session")
+
+test_that("canOpenPort", {
+  prxy <- proxy(bmpPort = 9090L, port = 39500L)
+  ports <- prxy  %>% getPorts
+  expect_true(inherits(ports[[1]], "proxy"))
+  expect_identical(ports[[1]]$port, 39500L)
+})
+
+test_that("canCloseProxy", {
+  prxy <- proxy(bmpPort = 9090L, port = 39500L)
+  ports <- prxy  %>% getPorts
+  expect_identical(length(ports), 1L)
+  prxy %>% closeProxy
+  ports <- prxy  %>% getPorts
+  expect_identical(length(ports), 0L)
+})
+
+test_that("canWaitForTrafficToBeQuiet", {
+  prxy <- proxy(bmpPort = 9090L, port = 39500L)
+  expect_silent(
+    prxy %>% waitForTraffic(quietPeriod = 2000L, timeOut = 5000L)
+  )
+})
+
+test_that("canSetTimeOuts", {
+  prxy <- proxy(bmpPort = 9090L, port = 39500L)
+  expect_silent(
+    prxy %>% timeouts(requestTimeout = 10000L, 
+                      connectionTimeout = 10000L,
+                      readTimeout = 10000L,
+                      dnsCacheTimeout = -1L)
+  )
+})
+
+test_that("canSetRetryCount", {
+  prxy <- proxy(bmpPort = 9090L, port = 39500L)
+  expect_silent(
+    prxy %>% setRetry(retryCount = 1L)
+  )
+})
+
+test_that("canEmptyDNSCache", {
+  prxy <- proxy(bmpPort = 9090L, port = 39500L)
+  expect_silent(
+    prxy %>% emptyDNS
+  )
+})
