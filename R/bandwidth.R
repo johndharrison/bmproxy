b57566b7ef858fb588970294964e32d9d8db1701
johndharrison <johndharrison0@gmail.com>
2016-10-23 04:42:50 -0700

    Add bandwidth function content

--- R/bandwidth.R
+++ R/bandwidth.R
@@ -1,22 +1,56 @@
 
 #' @rdname limitBandwidth
-limitBandwidth <- function(proxy, ...){
+limitBandwidth <- function(proxy, downK = NULL, upK = NULL, 
+                           downMaxK = NULL, upMaxK = NULL, latency = NULL, 
+                           enable = FALSE, payPercent = NULL, 
+                           maxBits = NULL, ...){
+  assert_proxy(proxy)
+  assert_integer_or_null(downK)
+  assert_integer_or_null(upK)
+  assert_integer_or_null(downMaxK)
+  assert_integer_or_null(upMaxK)
+  assert_integer_or_null(latency)
+  assert_that(is.logical(enable))
+  assert_integer_or_null(payPercent)
+  assert_integer_or_null(maxBits)
   wdata <- data.frame(port = proxy$port, stringsAsFactors = FALSE)
   path <- whisker.render(bmpAPI[['limitBandwidth']][['path']], wdata)
   appURL <- file.path(proxy$url, path)
-  VERB(verb = bmpAPI[['limitBandwidth']][['method']],
-       url = appURL)
+  body <- list()
+  body$downstreamKbps <- downK
+  body$upstreamKbps <- upK
+  body$downstreamMaxKB <- downMaxK
+  body$upstreamMaxKB <- upMaxK
+  body$latency <- latency
+  body$enable <- enable
+  body$payloadPercentage <- payPercent
+  body$maxBitsPerSecond <- maxBits
+  
+  res <- VERB(verb = bmpAPI[['limitBandwidth']][['method']],
+              url = appURL,
+              body = body,
+              encode = "form"
+  )
+  stop_for_status(res, paste("add URL black list returned response: ", 
+                             res$status)
+  )
+  invisible(proxy)
 }
 
 
 
 #' @rdname getBWremaining
 getBWremaining <- function(proxy, ...){
+  assert_proxy(proxy)
   wdata <- data.frame(port = proxy$port, stringsAsFactors = FALSE)
   path <- whisker.render(bmpAPI[['getBWremaining']][['path']], wdata)
   appURL <- file.path(proxy$url, path)
-  VERB(verb = bmpAPI[['getBWremaining']][['method']],
-       url = appURL)
+  res <- VERB(verb = bmpAPI[['getBWremaining']][['method']],
+              url = appURL)
+  stop_for_status(res, paste("add URL black list returned response: ", 
+                             res$status)
+  )
+  data.frame(content(res), stringsAsFactors = FALSE)
 }
 
 
