3df099142d35d13bc3366e13e3250212f23c24cb
johndharrison <johndharrison0@gmail.com>
2016-10-23 07:51:01 -0700

    Add intercept functions content

--- R/intercept.R
+++ R/intercept.R
@@ -1,22 +1,40 @@
 
 #' @rdname reqInterceptor
-reqInterceptor <- function(proxy, ...){
+reqInterceptor <- function(proxy, jsFilter, ...){
+  assert_proxy(proxy)
+  assert_that(is_string(jsFilter))
   wdata <- data.frame(port = proxy$port, stringsAsFactors = FALSE)
   path <- whisker.render(bmpAPI[['reqInterceptor']][['path']], wdata)
   appURL <- file.path(proxy$url, path)
-  VERB(verb = bmpAPI[['reqInterceptor']][['method']],
-       url = appURL)
+  res <- VERB(verb = bmpAPI[['reqInterceptor']][['method']],
+              url = appURL, 
+              body = jsFilter,
+              content_type_json()
+  )
+  stop_for_status(res, paste("request interceptor returned response: ", 
+                             res$status)
+  )
+  invisible(proxy)
 }
 
 
 
 #' @rdname resInterceptor
-resInterceptor <- function(proxy, ...){
+resInterceptor <- function(proxy, jsFilter, ...){
+  assert_proxy(proxy)
+  assert_that(is_string(jsFilter))
   wdata <- data.frame(port = proxy$port, stringsAsFactors = FALSE)
   path <- whisker.render(bmpAPI[['resInterceptor']][['path']], wdata)
   appURL <- file.path(proxy$url, path)
-  VERB(verb = bmpAPI[['resInterceptor']][['method']],
-       url = appURL)
+  res <- VERB(verb = bmpAPI[['resInterceptor']][['method']],
+              url = appURL, 
+              body = jsFilter,
+              content_type_json()
+  )
+  stop_for_status(res, paste("response interceptor returned response: ", 
+                             res$status)
+  )
+  invisible(proxy)
 }
 
 
