418a29629bcf383f5831d354ee942cd2143f2230
johndharrison <johndharrison0@gmail.com>
2016-10-23 09:11:44 -0700

    Test for BMP server

--- tests/testthat/test-misc_tests.R
+++ tests/testthat/test-misc_tests.R
@@ -2,7 +2,13 @@ context("misctests")
 
 test_that("canPrintProxy", {
   prxy <- proxy(bmpPort = 9090L, port = 39500L)
-  expect_silent(
+  expect_output(
     print(prxy)
+  )
+})
+
+test_that("shouldFailIfnoBMP", {
+  expect_error(
+    prxy <- proxy(bmpPort = 2222L, port = 39500L)
   )
 })
