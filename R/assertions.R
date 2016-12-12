182a211e505613135251127901fa94c72e44ac91
johndharrison <johndharrison0@gmail.com>
2016-12-12 08:50:55 -0800

    Add assertions

--- R/assertions.R
+++ R/assertions.R
@@ -19,3 +19,11 @@ assert_integer_or_null <- function(x) {
 assert_list_or_null <- function(x) {
   stopifnot(is.integer(x) || is.null(x))
 }
+
+is_integer <- function(x){
+  is.integer(x)
+}
+
+assertthat::on_failure(is_integer) <- function(call, env) {
+  paste0(deparse(call$x), " should be an integer value.")
+}
